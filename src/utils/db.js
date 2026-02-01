1ac9855d8ee590bc8a6dad1e892e836342417041
Sethuselvi <sethuselvi-98@gmail.com>
2026-02-01 18:17:45 +0530

    Added env file

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 let isConnected = false;
 export const connect = async () =>{
     if(isConnected) return;
-        await mongoose.connect('mongodb+srv://sethuselvi:1234@nodetuts.73kic02.mongodb.net/?appName=nodetuts/paste_bucket');
+        await mongoose.connect(process.env.MONGODB_URI);
           isConnected = true;
 }
